dd7b9e3e4cc64035790d82b927f5c01e179cf11f
ialexpovod <ialexefimenko@icloud.com>
2023-06-27 00:38:35 +0300

    change version and add url

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='fis-api',
-      version='0.1.12',
+      version='0.1.13',
       description='Fuzzy Interface System API',
       author='Alex Povod',
-      url='',
+      url='https://github.com/ialexpovod/fis/tree/main',
       license='MIT',
       packages=find_packages(),
       install_requires=[
